75a23c6e5736ead25351d96fdb70f65c72eb93b9
Jesse Swart <jesseswart08@gmail.com>
2023-01-07 17:39:06 +0100

    Changed some things to hopefully fix the issue

--- buttons.py
+++ buttons.py
@@ -2,7 +2,7 @@ def create_buttons(files: list[str]):
     html_string = ""
     for file in files:
         html_string += f"""
-        <form method="POST" enctype="multipart/form-data">
+        <form method=POST enctype=multipart/form-data>
             <input type=submit value="{file}" name="{file}"
             </form>
         """
--- server.py
+++ server.py
@@ -39,8 +39,9 @@ def download_file(filename):
 @app.route('/download', methods=['GET', 'POST'])
 def download_page():
     files = os.listdir(app.config['UPLOAD_FOLDER'])
-    print('DEBUG1')
+    print('DEBUG1') # This one gets printed to the console
     if request.method == 'POST':
+        print('DEBUG2') # This one does not get printed to the console
         for file in files:
             if request.form.get(f'{file}') == f'{file}':
                 return send_from_directory(app.config['UPLOAD_FOLDER'], f'{file}')
