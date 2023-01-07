63f3b4c7fb798a1f14c37a01cc45a06dbc1a83dc
Jesse Swart <jesseswart08@gmail.com>
2023-01-07 18:00:45 +0100

    Changed some things to hopefully fix the issue

--- server.py
+++ server.py
@@ -39,9 +39,12 @@ def download_file(filename):
 @app.route('/download', methods=['GET', 'POST'])
 def download_page():
     files = os.listdir(app.config['UPLOAD_FOLDER'])
-    print('DEBUG1') # This one gets printed to the console
-    print(f'{request.form}')
-    print(files)
+    print('DEBUG1')  # This one gets printed to the console
+    if request.method == 'POST':
+        print('DEBUG2')  # This one does not get printed to the console
+        for file in files:
+            if request.form.get(f'{file}') == f'{file}':
+                return send_from_directory(app.config['UPLOAD_FOLDER'], f'{file}')
     return create_buttons(files)
 
 
