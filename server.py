44a377a602cc2f0d96a89cedc35dc15d3290d6b8
Jesse Swart <jesseswart08@gmail.com>
2023-01-07 18:33:35 +0100

    Removed some comments

--- server.py
+++ server.py
@@ -48,13 +48,10 @@ def upload_file():
         return '<h1>Files Cleared</h1>'
     else:
         if request.method == 'POST':
-            # check if the post request has the file part
             if 'file' not in request.files:
                 flash('No file part')
                 return redirect(request.url)
             file = request.files['file']
-            # if user does not select file, browser also
-            # submit an empty part without filename
             if file.filename == '':
                 flash('No selected file')
                 return redirect(request.url)
