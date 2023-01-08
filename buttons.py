d8e3aa979660a7da9bf9b3f981d0c1a901dfdbb2
Jesse Swart <jesseswart08@gmail.com>
2023-01-08 09:49:20 +0100

    Made it so that now you can see if there are no files uploaded

--- buttons.py
+++ buttons.py
@@ -1,9 +1,12 @@
 def create_buttons(files: list[str]):
-    html_string = ""
-    for file in files:
-        html_string += f"""
-<form method=post enctype=multipart/form-data>
-<input type=submit value="{file}" name="{file}">
-</form>\n
-        """
-    return html_string
+    if len(files) != 0:
+        html_string = ""
+        for file in files:
+            html_string += f"""
+        <form method=post enctype=multipart/form-data>
+        <input type=submit value="{file}" name="{file}">
+        </form>\n
+                """
+        return html_string
+    else:
+        return '<h1>There are no files upload, go to <a href="/upload">/upload</a> and upload some files.'
