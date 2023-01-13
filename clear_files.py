daea3c3ecef689d4560f8f0648e1cca02b58e6e2
Reda-BENMAKDAD <redabenmakdad@gmail.com>
2023-01-13 22:52:10 +0100

    made clear function plateform independent

--- clear_files.py
+++ clear_files.py
@@ -5,5 +5,5 @@ def clear_files() -> str:
     folder = 'uploads'
     files = os.listdir(folder)
     for file in files:
-        os.system(f'rm {folder}/{file}')
+        os.remove(f'{folder}/{file}')
     return '<h1>Server files cleared</h1>'
