1422d3aac29fd4c899d165df034fd2dc880cf542
Jesse Swart <jesseswart08@gmail.com>
2023-01-13 21:03:37 +0100

    Reformatted a small thing

--- buttons.py
+++ buttons.py
@@ -5,8 +5,7 @@ def create_buttons(files: list[str]):
             html_string += f"""
         <form method=post enctype=multipart/form-data>
         <input type=submit value="{file}" name="{file}">
-        </form>\n
-                """
+        </form>\n"""
         return html_string
     else:
         return '<h1>There are no files upload, go to <a href="/upload">/upload</a> and upload some files.'
