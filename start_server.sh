a1f8540ad0a5580d0f6514e82afe17e50b35d712
Jesse Swart <jesseswart08@gmail.com>
2023-01-14 23:16:52 +0100

    Updated start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-source env/bin/activate
+venv_name="env"
+
+# check if the venv already exists
+if [ ! -d "$venv_name" ]; then
+    # create a new venv
+    python3 -m venv "$venv_name"
+fi
+
+# activate the venv
+source "$venv_name/bin/activate"
+
+# run the server
 python3 server.py
