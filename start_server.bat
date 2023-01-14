6fd09ee53ca16f8d59740d884652c7c4b77b8754
Jesse Swart <jesseswart08@gmail.com>
2023-01-14 10:00:54 +0100

    Now it should work all the time

--- start_server.bat
+++ start_server.bat
@@ -1,4 +1,6 @@
 @echo off
-set "venv_path=%cd%\env"
+if not exist env_windows (
+    python -m venv env_windows
+)
+set "venv_path=%cd%\env_windows"
 call %venv_path%\Scripts\activate.bat
-python %cd%\server.py
