125a164b41b4e30c84cbf7538db4974d95631fad
Jesse Swart <jesseswart08@gmail.com>
2023-01-14 10:03:28 +0100

    Fine-tuned start_server.bat

--- start_server.bat
+++ start_server.bat
@@ -1,6 +1,7 @@
 @echo off
 if not exist env_windows (
     python -m venv env_windows
+    pip install -r requirements.txt
 )
 set "venv_path=%cd%\env_windows"
 call %venv_path%\Scripts\activate.bat
