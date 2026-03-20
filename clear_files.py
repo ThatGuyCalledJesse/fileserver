203618a61305aea6797d87938fc0d668161ba736
Jesse <72275409+ThatGuyCalledJesse@users.noreply.github.com>
2026-03-20 08:39:52 +0100

    Update clear_files.py
    Rewrote code comments

--- clear_files.py
+++ clear_files.py
@@ -1,15 +1,11 @@
-# OS is used to list all files in the directory
 import os
 
 
-# This function takes all files in a directory and removes them one by one
-def clear_files() -> str:
+def clear_files() -> str: # Function must return a string
     # Set the 'folder' variable to uploads, which is the folder where our files are being stored
     folder = 'uploads'
-    # Make a list called 'files' and store all files in the directory in that variable
-    files = os.listdir(folder)
+    files = os.listdir(folder) # A list to store all listed files as a variable
     # For every file in the list
     for file in files:
         # Remove the file from the folder
         os.remove(f'{folder}/{file}')
-    # Then return a message saying that all files are removed
