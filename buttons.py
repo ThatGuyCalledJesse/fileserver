dc37d0db2e535ec674f03fc2b5c3b7866d89b143
Jesse <72275409+ThatGuyCalledJesse@users.noreply.github.com>
2026-03-20 08:42:43 +0100

    Update buttons.py
    Rewrote the comments

--- buttons.py
+++ buttons.py
@@ -1,17 +1,17 @@
-# This function takes a list[str] of files and adds HTML code for a button for every file when download is called
+# This part is incredibly primitive, but it works so I never really rewrote it. Maybe someday I'll try a more orthodox approach, when this inevitably breaks perhaps.
+
+# This function takes a list of strings (files) and adds HTML code for a button for every file when download is called
 def create_buttons(files: list[str]):
     # Only execute this code block if there are files in the list
     if len(files) != 0:
-        # Empty string that represents an HTML file
-        html_string = ""
-        # For every file in the list
-        for file in files:
-            # Add this HTML code for a button to the string
+        html_string = "" # This is the string that will hold the html the the page render
+        for file in files: # For each files in the list
+            # Add a button with the value and name set to the name of the file
             html_string += f"""
             <form method=post enctype=multipart/form-data>
             <input type=submit value="{file}" name="{file}">
             </form>\n"""
-            # When finished return the string
+        # When finished return the string
         return html_string
     # Else return a message saying that there are no files
     else:
